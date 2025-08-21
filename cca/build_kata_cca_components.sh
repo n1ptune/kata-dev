9bca821dc469901549312999d68da646a88a630a
Kevin Zhao <kevin.zhao@linaro.org>
2025-08-21 10:31:38 +0800

    Add build cca firmware
    Signed-off-by: Kevin Zhao <kevin.zhao@linaro.org>

--- cca/build_kata_cca_components.sh
+++ cca/build_kata_cca_components.sh
@@ -6,5 +6,5 @@ KATA_WORKDIR=${KATA_WORKDIR:-kata-containers}
 git clone -b add-cca https://github.com/kevinzs2048/kata-containers.git $KATA_WORKDIR
 cd $KATA_WORKDIR/tools/packaging/kata-deploy/local-build
 sudo rm build -rf
-make kernel-cca-confidential-tarball shim-v2-tarball qemu-cca-experimental-tarball rootfs-cca-confidential-image-tarball rootfs-cca-confidential-initrd-tarball
+make kernel-cca-confidential-tarball shim-v2-tarball qemu-cca-experimental-tarball rootfs-cca-confidential-image-tarball rootfs-cca-confidential-initrd-tarball ovmf-cca-tarball
 echo "The generated Kata tar files are in build directory"
